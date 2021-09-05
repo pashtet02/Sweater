310c9201d941b31fed0048d0ed63a5f45e98a8a6
Pavlo Khodachok <Pavlo_Khodachok@epam.com>
2021-09-05 12:22:29 +0300

    added saving sessions to DB and captcha

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -4,9 +4,10 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
 
 @Entity
-public class Message {
+public class Message implements Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
---
+++ src/main/java/com/example/sweater/domain/dto/CaptchaResponseDto.java
@@ -0,0 +1,37 @@
+package com.example.sweater.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Set;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CaptchaResponseDto {
+    private boolean success;
+    @JsonAlias("error-codes")
+    private Set<String> errorCodes;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Set<String> getErrorCodes() {
+        return errorCodes;
+    }
+
+    public void setErrorCodes(Set<String> errorCodes) {
+        this.errorCodes = errorCodes;
+    }
+
+    @Override
+    public String toString() {
+        return "CaptchaResponseDto{" +
+                "success=" + success +
+                ", errorCodes=" + errorCodes +
+                '}';
+    }
+}
